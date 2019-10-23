6b106aef7d824c6c3af902512161920d27565a60
unknown <vivekdhakan007@gmail.com>
2019-10-23 17:39:25 +0530

    First commit

---
+++ ChatRoom/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChatRoom
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+    }
+}
